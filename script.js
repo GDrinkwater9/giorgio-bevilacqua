14c8bf7c019ae4a550ffdae4f70424ff2266ffa3
GDrinkwater9 <giorgio.bevilacqua00@gmail.com>
2022-11-24 16:47:20 +0100

    giorgio-bevilacqua

--- script.js
+++ script.js
@@ -11,11 +11,21 @@ AOS.init();
 
 const nav = document.querySelector('nav');
 
+
+
 window.addEventListener('scroll',() => {
-    if(window.pageYOffset > 70){
-        nav.classList.add('navbar-scrolled');  
+    if(window.pageYOffset > 70){  
+        nav.classList.add('navbar-scrolled');      
     }else{
         nav.classList.remove('navbar-scrolled');
     }
 });
 
+const menu = document.querySelector('.navbar-collapse')
+
+window.addEventListener('click',(e) => {
+    console.log(e.path.includes(nav));
+    if(!e.path.includes(nav)){
+        menu.classList.remove('show')
+    }
+} )
