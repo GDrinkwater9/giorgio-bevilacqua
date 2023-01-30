a39d47e31b2db7ae990227a74c69349b91e025b1
GDrinkwater9 <giorgio.bevilacqua00@gmail.com>
2023-01-30 19:01:13 +0100

    CV

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ let typed = new Typed(".auto-type",{
 AOS.init();
 
 
+
+
 const nav = document.querySelector('nav');
-
-
 
 window.addEventListener('scroll',() => {
     if(window.pageYOffset > 70){  
@@ -31,3 +31,32 @@ window.addEventListener('click',(e) => {
 } )
 
 
+
+
+function fetchDown (url, saveas) {
+    fetch(url)
+   
+    .then((result) => {
+      if (result.status != 200) { throw new Error("Bad server response"); }
+      return result.blob();
+    })
+   
+
+    .then((data) => {
+
+      console.log(data);
+   
+
+      var url = window.URL.createObjectURL(data),
+      anchor = document.createElement("a");
+      anchor.href = url;
+      anchor.download = saveas;
+      anchor.click();
+   
+
+      window.URL.revokeObjectURL(url);
+      document.removeChild(anchor);
+    })
+
+    .catch((error) => { console.log(error); })
+}
